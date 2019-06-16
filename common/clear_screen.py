c2957db559707397ed34885acbd524723f219c44
Peter Pallen <peter@ppallen.be>
2019-06-16 15:01:10 +0200

    Added info related to systens on which to use
    Signed-off-by: Peter Pallen <peter@ppallen.be>

--- common/clear_screen.py
+++ common/clear_screen.py
@@ -2,6 +2,6 @@ import os
 
 
 def clear_screen():
-    """clear the data on the screen.
+    """clear the data on the screen. for windows and unix/linux systems
     """
     os.system("cls" if os.name == "nt" else "clear")
